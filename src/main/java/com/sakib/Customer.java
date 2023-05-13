a3a6177d34abb451bef1a0212965789a53a94a45
Sakib Sadman <ssadman3659@gmail.com>
2023-05-13 20:57:46 -0300

    Integrated docker

---
+++ src/main/java/com/sakib/Customer.java
@@ -0,0 +1,46 @@
+package com.sakib;
+
+import java.util.Objects;
+
+public class Customer {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+
+
+    public Customer(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, age);
+    }
+}
